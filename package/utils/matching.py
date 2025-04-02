88e86e9f49edd5b51ce5923ea74aaab18b4f7ad9
Sandrine Nokam <sandrinewato27@gmail.com>
2025-04-02 14:44:37 +0200

    Ajout de la fonction de matching qui prend en entrée un chemin d'accès vers le releve bancaire et un dataframe contenant les informations sur les factures

--- package/utils/matching.py
+++ package/utils/matching.py
@@ -5,3 +5,83 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 from package.utils.mistral_model import *
+from package.utils.file_operations import get_bank_statement
+from config import *
+from sentence_transformers import SentenceTransformer, util
+
+MODEL_EMBEDED = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+
+
+def matching_func(bank_statement_path, receipts):
+    df_bank_statement = get_bank_statement(bank_statement_path)
+    df_bank_statement_copy = df_bank_statement.copy()
+    df_bank_statement_copy["receipt"] = None  # Colonne pour lier les reçus
+
+    for i, receipt in receipts.iterrows():
+        # 1. Filtrer les lignes avec le même montant
+        matches_amount = df_bank_statement_copy[df_bank_statement_copy["amount"] == receipt["amount"]]
+
+        if not matches_amount.empty:
+            if len(matches_amount) == 1:
+                index = matches_amount.index[0]
+                df_bank_statement_copy.at[index, "receipt"] = receipt["filename"]
+                continue
+
+            # 2. Affiner avec la date
+            matches_date = matches_amount[matches_amount["date"] == receipt["date"]]
+
+            if len(matches_date) == 1:
+                index = matches_date.index[0]
+                df_bank_statement_copy.at[index, "receipt"] = receipt["filename"]
+                continue
+
+            # 3. Si plusieurs lignes encore : fuzzy matching sur nom du marchand
+            subset = matches_date if not matches_date.empty else matches_amount
+
+            receipt_name = receipt.get("merchant_name", "")
+            receipt_address = receipt.get("address", "")
+            vendor_receipt = receipt_name + " " + receipt_address if receipt_address else receipt_name
+
+            if not vendor_receipt.strip():
+                continue  # Pas d'infos pour matcher
+
+            vendor_receipt_embed = MODEL_EMBEDED.encode(vendor_receipt, convert_to_tensor=True)
+            similarities = []
+
+            for j, row in subset.iterrows():
+                vendor_name = row.get("vendor", "")
+                vendor_embed = MODEL_EMBEDED.encode(vendor_name, convert_to_tensor=True)
+                score = util.cos_sim(vendor_embed, vendor_receipt_embed).item()
+                similarities.append((j, score))
+
+            if similarities:
+                best_index, best_score = max(similarities, key=lambda x: x[1])
+                if best_score > 0.75:
+                    df_bank_statement_copy.at[best_index, "receipt"] = receipt["filename"]
+
+    return df_bank_statement_copy
+ 
+
+
+##### TEST #####
+
+
+
+results = analyzed_receipts(IMAGES_PATH, CONTEXT_PATH, PROMPT_PATH, MODEL, CLIENT)
+
+receipts = parse_json_to_dataframe(results)
+
+print(type(receipts))
+print(receipts.info())
+
+df_matched = matching_func(BANK_STATEMENT_PATH, receipts)
+
+
+print(df_matched)
+print(df_matched.info())
+print("##########################")
+print("##########################")
+print(df_matched.head(10))
+
+
